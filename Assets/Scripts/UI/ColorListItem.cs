b6349e97881f6d4ff0e9ab5f2716d39bdc2482e8
Jew <dima.com655@gmail.com>
2022-07-20 12:16:38 +0300

    Add Update Point + Quality Group

--- Assets/Scripts/UI/ColorListItem.cs
+++ Assets/Scripts/UI/ColorListItem.cs
@@ -5,41 +5,41 @@ using UnityEngine.UI;
 
 namespace BizzyBeeGames.PictureColoring
 {
-	public class ColorListItem : ClickableListItem
-	{
-		#region Inspector Variables
+    public class ColorListItem : ClickableListItem
+    {
+        #region Inspector Variables
 
-		[SerializeField] private Image 		colorImage		= null;
-		[SerializeField] private Text		numberText		= null;
-		[SerializeField] private GameObject	completedObj	= null;
-		[SerializeField] private GameObject	selectedObj		= null;
+        [SerializeField] private Image colorImage = null;
+        [SerializeField] private Text numberText = null;
+        [SerializeField] private GameObject completedObj = null;
+        [SerializeField] private GameObject selectedObj = null;
 
-		#endregion
+        #endregion
 
-		#region Public Methods
+        #region Public Methods
 
-		public void Setup(Color color, int number)
-		{
-			colorImage.color	= color;
-			numberText.text		= number.ToString();
+        public void Setup(Color color, int number)
+        {
+            colorImage.color = color;
+            numberText.text = number.ToString();
 
-			numberText.enabled = true;
+            numberText.enabled = true;
 
-			selectedObj.SetActive(false);
-			completedObj.SetActive(false);
-		}
+            selectedObj.SetActive(false);
+            completedObj.SetActive(false);
+        }
 
-		public void SetSelected(bool isSelected)
-		{
-			selectedObj.SetActive(isSelected);
-		}
+        public void SetSelected(bool isSelected)
+        {
+            selectedObj.SetActive(isSelected);
+        }
 
-		public void SetCompleted()
-		{
-			numberText.enabled = false;
-			completedObj.SetActive(true);
-		}
+        public void SetCompleted()
+        {
+            numberText.enabled = false;
+            completedObj.SetActive(true);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
