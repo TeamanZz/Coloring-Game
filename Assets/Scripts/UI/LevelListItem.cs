8c710f563fa643ee027fe14e28373805c4da6023
Jew <dima.com655@gmail.com>
2022-07-25 22:07:32 +0300

    Add Fucking Border

--- Assets/Scripts/UI/LevelListItem.cs
+++ Assets/Scripts/UI/LevelListItem.cs
@@ -5,127 +5,127 @@ using UnityEngine.UI;
 
 namespace BizzyBeeGames.PictureColoring
 {
-	public class LevelListItem : RecyclableListItem<LevelData>
-	{
-		#region Inspector Variables
+    public class LevelListItem : RecyclableListItem<LevelData>
+    {
+        #region Inspector Variables
 
-		[SerializeField] private PictureCreator	pictureCreator		= null;
-		[SerializeField] private GameObject		loadingIndicator	= null;
-		[SerializeField] private GameObject		completedIndicator	= null;
-		[SerializeField] private GameObject		playedIndicator		= null;
-		[SerializeField] private GameObject		lockedIndicator		= null;
-		[SerializeField] private GameObject		coinCostContainer	= null;
-		[SerializeField] private Text			coinCostText		= null;
+        [SerializeField] private PictureCreator pictureCreator = null;
+        [SerializeField] private GameObject loadingIndicator = null;
+        [SerializeField] private GameObject completedIndicator = null;
+        [SerializeField] private GameObject playedIndicator = null;
+        [SerializeField] private GameObject lockedIndicator = null;
+        [SerializeField] private GameObject coinCostContainer = null;
+        [SerializeField] private Text coinCostText = null;
 
-		#endregion
+        #endregion
 
-		#region Member Variables
+        #region Member Variables
 
-		private string	levelId;
-		private int		loadId;
-		private bool	isLoading;
+        private string levelId;
+        private int loadId;
+        private bool isLoading;
 
-		#endregion
+        #endregion
 
-		#region Public Methods
+        #region Public Methods
 
-		public override void Initialize(LevelData dataObject)
-		{
-		}
+        public override void Initialize(LevelData dataObject)
+        {
+        }
 
-		public override void Removed()
-		{
-			if (isLoading)
-			{
-				LoadManager.Instance.Cancel(levelId, loadId);
+        public override void Removed()
+        {
+            if (isLoading)
+            {
+                LoadManager.Instance.Cancel(levelId, loadId);
 
-				isLoading = false;
+                isLoading = false;
 
-				pictureCreator.Clear();
-			}
-		}
+                pictureCreator.Clear();
+            }
+        }
 
-		public override void Setup(LevelData levelData)
-		{
-			UpdateUI(levelData);
+        public override void Setup(LevelData levelData)
+        {
+            UpdateUI(levelData);
 
-			levelId	= levelData.Id;
-			loadId	= LoadManager.Instance.LoadLevel(levelData, OnLoadManagerFinished);
+            levelId = levelData.Id;
+            loadId = LoadManager.Instance.LoadLevel(levelData, OnLoadManagerFinished);
 
-			if (loadId == 0)
-			{
-				// loadId of 0 means the LevelData is already loaded and ready to use
-				SetImages(levelData);
+            if (loadId == 0)
+            {
+                // loadId of 0 means the LevelData is already loaded and ready to use
+                SetImages(levelData);
 
-				loadingIndicator.SetActive(false);
-			}
-			else
-			{
-				// Hide the images while the level is loading
-				pictureCreator.Clear();
+                loadingIndicator.SetActive(false);
+            }
+            else
+            {
+                // Hide the images while the level is loading
+                pictureCreator.Clear();
 
-				// LevelLoadManager is loading the data needed to display the thumbnail
-				isLoading = true;
+                // LevelLoadManager is loading the data needed to display the thumbnail
+                isLoading = true;
 
-				loadingIndicator.SetActive(true);
-			}
-		}
+                loadingIndicator.SetActive(true);
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region Private Methods
+        #region Private Methods
 
-		/// <summary>
-		/// Invoked when the LevelLoadManager finishes loading everything needed to display the levels thumbnail
-		/// </summary>
-		private void OnLoadManagerFinished(LevelData levelData, bool success)
-		{
-			isLoading = false;
+        /// <summary>
+        /// Invoked when the LevelLoadManager finishes loading everything needed to display the levels thumbnail
+        /// </summary>
+        private void OnLoadManagerFinished(LevelData levelData, bool success)
+        {
+            isLoading = false;
 
-			loadingIndicator.SetActive(false);
+            loadingIndicator.SetActive(false);
 
-			if (success)
-			{
-				SetImages(levelData);
-			}
-		}
+            if (success)
+            {
+                SetImages(levelData);
+            }
+        }
 
-		/// <summary>
-		/// Sets the images
-		/// </summary>
-		private void SetImages(LevelData levelData)
-		{
-			LevelFileData levelFileData = levelData.LevelFileData;
+        /// <summary>
+        /// Sets the images
+        /// </summary>
+        private void SetImages(LevelData levelData)
+        {
+            LevelFileData levelFileData = levelData.LevelFileData;
 
-			float containerWidth	= (pictureCreator.transform.parent as RectTransform).rect.width;
-			float containerHeight	= (pictureCreator.transform.parent as RectTransform).rect.height;
-			float contentWidth		= levelFileData.imageWidth;
-			float contentHeight		= levelFileData.imageHeight;
-			float scale				= Mathf.Min(containerWidth / contentWidth, containerHeight / contentHeight, 1f);
+            float containerWidth = (pictureCreator.transform.parent as RectTransform).rect.width;
+            float containerHeight = (pictureCreator.transform.parent as RectTransform).rect.height;
+            float contentWidth = levelFileData.imageWidth;
+            float contentHeight = levelFileData.imageHeight;
+            float scale = Mathf.Min(containerWidth / contentWidth, containerHeight / contentHeight, 1f);
 
-			pictureCreator.RectT.sizeDelta	= new Vector2(contentWidth, contentHeight);
-			pictureCreator.RectT.localScale	= new Vector3(scale, scale, 1f);
+            pictureCreator.RectT.sizeDelta = new Vector2(contentWidth, contentHeight);
+            pictureCreator.RectT.localScale = new Vector3(scale, scale, 1f);
 
-			pictureCreator.Setup(levelData);
-		}
+            pictureCreator.Setup(levelData);
+        }
 
-		/// <summary>
-		/// Updates the UI of the list item
-		/// </summary>
-		private void UpdateUI(LevelData levelData)
-		{
-			bool isLocked 		= levelData.locked && !levelData.LevelSaveData.isUnlocked;
-			bool isPlaying		= GameManager.Instance.IsLevelPlaying(levelData.Id);
-			bool isCompleted	= levelData.LevelSaveData.isCompleted;
+        /// <summary>
+        /// Updates the UI of the list item
+        /// </summary>
+        private void UpdateUI(LevelData levelData)
+        {
+            bool isLocked = levelData.locked && !levelData.LevelSaveData.isUnlocked;
+            bool isPlaying = GameManager.Instance.IsLevelPlaying(levelData.Id);
+            bool isCompleted = levelData.LevelSaveData.isCompleted;
 
-			completedIndicator.SetActive(isCompleted);
-			playedIndicator.SetActive(!isCompleted && isPlaying);
-			lockedIndicator.SetActive(isLocked);
-			coinCostContainer.SetActive(isLocked);
+            completedIndicator.SetActive(isCompleted);
+            playedIndicator.SetActive(!isCompleted && isPlaying);
+            lockedIndicator.SetActive(isLocked);
+            coinCostContainer.SetActive(isLocked);
 
-			coinCostText.text = levelData.coinsToUnlock.ToString();
-		}
+            coinCostText.text = levelData.coinsToUnlock.ToString();
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
